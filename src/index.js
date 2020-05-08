666224cd965cfb566107a3e6be7bbadbde63e1ba
Jose Angeles <humblekgames@gmail.com>
2020-05-08 17:30:15 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res)=> {
     res.send('Server up and running');
 })
 
-app.listen(8080, () => {
-    console.log('Project1 listening on http://localhost:8080');
+app.listen(5000, () => {
+    console.log('Project1 listening on http://localhost:5000');
     
 });
