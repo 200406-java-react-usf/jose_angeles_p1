4fdb11d337ef39073a7342a2e619cadf14c9fbb4
Jose Angeles <humblekgames@gmail.com>
2020-05-15 15:37:29 -0400

    Added comments on user-service and fixed scripts in json file

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -13,31 +13,50 @@ export class UserService {
     constructor(private userRepository: UserRepository) {
         this.userRepository = userRepository;
     }
+
+    // the following functions will validate data and call the userRepo
     async getAllUsers(): Promise<User[]> {
+        // call getAll() 
         let users = await this.userRepository.getAll();
+
+        // make sure that what we got is not empty
         if (users.length == 0) {
             throw new ResourceNotFoundError('There aren\'t any users in the DB');
         }
+
+        // return all the users
         return users;       
     }
 
     async getUserById(id: number): Promise<User> {
+        // validate id to make sure id is a number
         if (!isValidId(id)) {
             throw new BadRequestError('This is not a valid id');
         };
+
+        // call getById from userRepo
         let user = await this.userRepository.getById(id);
+
+        // make sure what we got does exist
         if (isEmptyObject(user)) {
             throw new ResourceNotFoundError('There aren\'t any user with given id');
         };
+
+        // just return our user
         return user;
     }
 
     async addNewUser(newUser: User): Promise<User> {
         try {
+            // is our input a valid User?
             if (!isValidObject(newUser, 'username')) {
                 throw new BadRequestError('Invalid property value found in provided user');
             }
+
+            // run addNew from user repo and store that value
             const persistedUser = await this.userRepository.addNew(newUser);
+
+            // return stored value
             return persistedUser;
         } catch (e) {
             throw e;
@@ -46,10 +65,15 @@ export class UserService {
 
     async updateUser(updateUser: User): Promise<boolean> {
         try {
+            // is our input a valid User?
             if (!isValidObject(updateUser, 'id')) {
                 throw new BadRequestError('Invalid properly value found in provided user');
             }
+
+            // run update from user repo and store that value
             const persistedUser = await this.userRepository.update(updateUser);
+
+            // return stored value
             return persistedUser;
         } catch (e) {
             throw e;
@@ -57,18 +81,21 @@ export class UserService {
     }
 
     async deleteUserById(jsonObj: object): Promise<boolean> {
+        // in the next 3 lines we extract the id from the object passed
         let keys = Object.keys(jsonObj);
         let val = keys[0];
         let userId = +jsonObj[val];
+
         try {
+            // validate if id is a number
             if (!isValidId(userId)) {
                 throw new BadRequestError('Invalid id provided');
             }
+
+            // call deleteById on user repos and store boolean
             let deletedUser = await this.userRepository.deleteById(userId);
 
-            if (!deletedUser) {
-                throw new ResourceNotFoundError('The provided id doesn\'t exist or has already been deleted')
-            }
+            // just return the boolean which is most likely true
             return deletedUser;
         } catch (e) {
             throw e;
